2e5c08f38f4aead1e515c4826a91f01174e5d1f4
Manu-RMT <68026496+Manu-RMT@users.noreply.github.com>
2023-10-20 14:22:09 +0200

    Ajout équilibrage datasets

--- Projet.py
+++ Projet.py
@@ -5,10 +5,10 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from ModuleFonction import *
 # nom des datasets
-# all_datasets_name = ["abalone8","abalone17","abalone20","autompg",
-#                   "australian","balance","bankmarketing","bupa","german","glass",
-#                   "hayes","heart","iono","libras",'newthyroid',"pageblocks","pima","satimage","sonar",
-#                   "spambase","splice","vehicle","wdbc","wine",'wine4',"yeast3","yeast6"]
+all_datasets_name = ["abalone8","abalone17","abalone20","autompg",
+                  "australian","balance","bankmarketing","bupa","german","glass",
+                  "hayes","heart","iono","libras",'newthyroid',"pageblocks","pima","satimage","sonar",
+                   "spambase","splice","vehicle","wdbc","wine",'wine4',"yeast3","yeast6"]
 
 # all_datasets_name = ["abalone8","abalone17","abalone20","autompg",
 #                   "balance","bupa","german","glass",
@@ -16,7 +16,7 @@ from ModuleFonction import *
 #                   "spambase","splice","vehicle","wdbc","wine",'wine4',"yeast3","yeast6"]
 
 # "bankmarketing","australian", "pageblocks", 
-all_datasets_name = ["pima"]
+#all_datasets_name = ["pima"]
 # dictionnaire de stockage des resultats
 stock_resultat = {}
 # liste contenant tous les datasets
@@ -24,11 +24,17 @@ dfs = {}
 for name in all_datasets_name:
     dfs[name]=data_recovery(name)
     stock_resultat[name] = {}
-    #repartition de y
+    # repartition de y
     stock_resultat[name]["repartition_y"] =  dfs[name][1].mean()
+    # dataset équilibré ?
+    if dfs[name][1].mean() < 0.2 or dfs[name][1].mean() > 0.8 :
+        stock_resultat[name]["équilibré"] = False
+    else : 
+        stock_resultat[name]["équilibré"] = True
+        
     
     # SVM linear
-    stock_resultat[name]['SVM linear'] =  SVM_Test(dfs[name],name)
+    #stock_resultat[name]['SVM linear'] =  SVM_Test(dfs[name],name)
     # Algorithme 2
 
 
